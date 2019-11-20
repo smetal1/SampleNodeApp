af2c1a18c679b83eb0f0b5799b49c2d5ae73d9b7
saurav kumar <saurav.kumar@kuliza.com>
2019-11-20 12:30:36 +0530

    updated the url

--- test/test-pages.js
+++ test/test-pages.js
@@ -2,7 +2,7 @@ var expect  = require('chai').expect;
 var request = require('request');
 
 it('Main page content', function(done) {
-    request('http://0.0.0.0:3000' , function(error, response, body) {
+    request('http://139.59.59.210:3000' , function(error, response, body) {
         expect(body).to.equal('Hello World');
         done();
     });
